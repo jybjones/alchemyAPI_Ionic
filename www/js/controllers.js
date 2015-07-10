0360c8e10de61cbe52f2c28e3dc5412281f96974
James Logan <jamesblogan20@gmail.com>
2015-07-10 11:24:13 -0500

    sorting function created, button added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,22 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.run(function ($rootScope) {
+    $rootScope.$on('$routeChangeStart', function (event, nextRoute) {
+      // var fb = new Firebase(API_URL);
+      // $rootScope.auth = fb.getAuth();
 
-.controller('ChatsCtrl', function($scope, Chats) {
+      // $rootScope.pos = $rootScope.pos;
+      // $rootScope.neg = $rootScope.neg;
+
+    });
+
+  })
+
+.controller('DashCtrl', function($scope, $rootScope) {
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats, $rootScope) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,10 +24,33 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+
+  // $scope.chats = Chats.all();
+  // $scope.remove = function(chat) {
+  //   Chats.remove(chat);
+  // }
+
+  $scope.params = {
+    term: "",
+    type: ""
+  }
+
+
+  $scope.positive = []
+  $scope.negative = []
+
+  $scope.sortPosNeg = function (data) {
+
+    data.result.docs.forEach(function (article) {
+      if (article.source.enriched.url.docSentiment.type = "positive") {
+        $scope.positive.push(article);
+      } else if (article.source.enriched.url.docSentiment.type = "negative") {
+        $scope.negative.push(article)
+      }
+    })
+
+    $rootScope.pos = $scope.positive;
+    $rootScope.neg = $scope.negative;
   }
 })
 
@@ -21,8 +58,6 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope, $rootScope) {
+  $scope.pos = $rootScope.pos;
 });
